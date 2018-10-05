24843cab3f060f46806f515943603731c5b5b690
Amit Kucheria <amit.kucheria@linaro.org>
2018-10-06 01:53:59 +0530

    thermal: qcom: lmh: add debug support
    Signed-off-by: Amit Kucheria <amit.kucheria@linaro.org>

---
+++ drivers/thermal/qcom/lmh_dbg.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2014-2017, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef __QCOM_LMH_H__
+#define __QCOM_LMH_H__
+
+#include <linux/platform_device.h>
+
+int lmh_debug_register(struct platform_device *pdev);
+
+#endif /* __QCOM_LMH_H__ */
